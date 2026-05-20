32405b8867e5cff56200f169e7bcd419e7226542
Pradeep Bugga <pradeep.bugga0@gmail.com>
2026-05-19 22:38:25 -0400

    added new function for combine and sort that doesn't sent heavy abstract / phr

--- core/search/combine.py
+++ core/search/combine.py
@@ -47,3 +47,46 @@ def combine_and_sort(docs, scores):
     results.sort(key=lambda x: x["score"], reverse=True)
     return results
 
+def combine_and_sort_semantic_filter(docs, scores):
+    
+    assert len(docs) == len(scores)
+
+    results = []
+    for doc, score in zip(docs, scores):
+        if score > MIN_SCORE_THRESHOLD:
+            results.append({
+                "grant_id": doc["grant_id"],
+                "title": doc["title"],
+                #"subproject_id": doc["subproject_id"],
+                #"core_project_num": doc["core_project_num"],
+                "fiscal_year": doc["fiscal_year"],
+                "amount": doc["amount"],
+                #"abstract": doc["abstract"],
+                #"phr": doc["phr"],
+                "agency_ic": doc["agency_ic"],
+                #"project_start_date": doc["project_start_date"],
+                #"project_end_date": doc["project_end_date"],
+                #"budget_start_date": doc["budget_start_date"],
+                #"budget_end_date": doc["budget_end_date"],
+                "org_name": doc["org_name"],
+                "org_city": doc["org_city"],
+                "org_state": doc["org_state"],
+                "org_country": doc["org_country"],
+                "pi_first_name": doc["pi_first_name"],
+                "pi_middle_name": doc["pi_middle_name"],
+                "pi_last_name": doc["pi_last_name"],
+                "score": float(score),
+                #"vector_similarity": doc.get("vector_similarity", 0.0),
+                "mechanistic": doc.get("mechanistic", None),
+                "therapeutic": doc.get("therapeutic", None),
+                "diagnostic": doc.get("diagnostic", None),
+                "clinical": doc.get("clinical", None),
+                "research_tool": doc.get("research_tool", None),
+                "infrastructure": doc.get("infrastructure", None),
+                "education": doc.get("education", None),
+                "obs_ep": doc.get("obs_ep", None),
+            })
+        
+    results.sort(key=lambda x: x["score"], reverse=True)
+    return results
+
