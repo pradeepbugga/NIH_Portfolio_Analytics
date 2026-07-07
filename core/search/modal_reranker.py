d2212c4bcc1d49415953b3bea72aa818025be2f6
Pradeep Bugga <pradeep.bugga0@gmail.com>
2026-07-07 00:57:26 -0400

    add distributed rerank fn

--- core/search/modal_reranker.py
+++ core/search/modal_reranker.py
@@ -5,3 +5,13 @@ rerank_fn = modal.Function.from_name(
     "nih-reranker",
     "Reranker.rerank_batch"
 )
+
+distributed_rerank_fn = modal.Function.from_name(
+    "nih-reranker",
+    "distributed_rerank"
+)
+
+rerank_test = modal.Function.from_name(
+    "nih-reranker",
+    "Reranker.debug_score"
+)
