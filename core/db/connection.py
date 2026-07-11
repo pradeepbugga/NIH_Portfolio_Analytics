0becd1173a287276254b58027bd7cfed835f6341
Pradeep Bugga <pradeep.bugga0@gmail.com>
2026-07-11 03:05:12 -0400

    removed unnecessary functions and imports, added docstring comment

--- core/db/connection.py
+++ core/db/connection.py
@@ -1,12 +1,16 @@
-# this script provides functions to get database connections for PostgreSQL, SQLite, and SQLAlchemy
-
 import psycopg2
-import sqlite3
-import sqlalchemy
 from core.config import POSTGRES_CONFIG
 
 
 def get_db_connection():
+
+    """
+    Get a connection to the PostgreSQL database using psycopg2.
+
+    Returns:
+        psycopg2.extensions.connection: A connection object to the PostgreSQL database.
+    """
+
     conn = psycopg2.connect(**POSTGRES_CONFIG)
     cur = conn.cursor()
 
@@ -15,13 +19,3 @@ def get_db_connection():
     
     return conn
 
-
-def get_sqlite_connection(db_path: str):
-    conn = sqlite3.connect(db_path)
-    return conn
-
-def get_sqlalchemy_engine():
-    return sqlalchemy.create_engine(
-        f"postgresql+psycopg2://{POSTGRES_CONFIG['user']}:{POSTGRES_CONFIG['password']}@{POSTGRES_CONFIG['host']}:{POSTGRES_CONFIG['port']}/{POSTGRES_CONFIG['dbname']}",
-        pool_pre_ping=True
-    )
