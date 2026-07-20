a387a9b98ca51678179aa4f0f08c3638f3520715
Pradeep Bugga <pradeep.bugga0@gmail.com>
2026-07-19 23:11:31 -0400

    initial add

---
+++ core/constants.py
@@ -0,0 +1,10 @@
+ONTOLOGY_LABELS = [
+    "Mechanistic / Basic Science",
+    "Therapeutic",
+    "Diagnostic",
+    "Research Tool",
+    "Clinical / Health Systems",
+    "Research Infrastructure",
+    "Education / Training",
+    "Observational Epidemiology",
+]
