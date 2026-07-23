998150a459c73ed965317a3be506dd93d4c692cd
Pradeep Bugga <pradeep.bugga0@gmail.com>
2026-07-23 18:54:24 -0400

    adjust naming to reflect new SDK naming

--- core/search/modal_reranker.py
+++ core/search/modal_reranker.py
@@ -1,14 +1,12 @@
 import modal
 
+# look up the class and its methods by name
 Reranker_cls = modal.Cls.from_name("nih-reranker", "Reranker")
-
-# create a reranker instance 
 reranker = Reranker_cls()
 
-# access class methods
-
 rerank_fn = reranker.rerank_batch
-
 rerank_test = reranker.debug_score
 
-distributed_rerank_fn = modal.Function.lookup("nih-reranker", "distributed_rerank")
+# standalone function for distributed reranking
+
+distributed_rerank_fn = modal.Function.from_name("nih-reranker", "distributed_rerank")
